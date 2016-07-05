39371694b7d84aef887a094fa49730dfdac65ad9
Gary Clayburg <gclaybur@comcast.net>
2016-07-05 10:30:29 -0600

    added docker support to build slave node

--- bounce.sh
+++ bounce.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker-compose stop && docker-compose build && docker-compose up -d jenkinsdata jslavedata jenkinsmaster jenkinsnginx && docker-compose logs
+#DOCKERGID is needed for "docker-compose build" to make sure jenkins can run docker commands inside container
+# We are assuming that the group id for the docker user on the host that runs "docker-compose build" is the same as the host that runs the containers
+export DOCKERGID=$(id -g docker)
+docker-compose stop && docker-compose build && docker-compose up -d jenkinsdata jslavedata jenkinsmaster jenkinsnginx sshcoderepo && docker-compose logs
