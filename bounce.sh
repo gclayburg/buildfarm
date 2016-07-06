fb0939423076416023283517c160e75e4ef85dbc
Gary Clayburg <gclaybur@comcast.net>
2016-07-06 17:53:10 -0600

    force ssh key generation on first use

--- bounce.sh
+++ bounce.sh
@@ -5,4 +5,15 @@ export DOCKERGID=$(id -g docker)
 
 #BUILDFARM_HOST is used to configure Jenkins automatic builds on git push.  See ./ssh-data/post-receive.
 export BUILDFARM_HOSTNAME="$(hostname)"
+
+cd $(dirname $0)
+
+#generate ssh keys for all servers, if we don't have a jenkins master key already, i.e. the first time this is run
+if [[ ! -f keys/jenkins-master-key ]]; then
+  if ! keys/genkey.sh ; then 
+    echo "ERROR generating ssh keys.  Fix this manually before starting buildfarm." 
+    exit 1
+  fi
+  echo "new ssh key generated for jenkins master"
+fi
 docker-compose stop && docker-compose build && docker-compose up -d jenkinsdata jslavedata jenkinsmaster jenkinsnginx sshcoderepo && docker-compose logs
