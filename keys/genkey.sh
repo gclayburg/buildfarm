e5d76e148f6e505db4829101d45e516ea894ffc9
Gary Clayburg <gclaybur@comcast.net>
2016-07-06 17:04:04 -0600

    updated keygen to include other identities

--- keys/genkey.sh
+++ keys/genkey.sh
@@ -8,6 +8,9 @@ cp jenkins-master-key.pub ../jenkins-data/jenkins-master-key.pub
 cat jenkins-master-key.pub >> authorized_keys
 
 #insert my own keys everywhere to ease manual troubleshooting
+[ -r ~/.ssh/id_dsa.pub ] && cat ~/.ssh/id_dsa.pub >> authorized_keys
+[ -r ~/.ssh/id_ecdsa.pub ] && cat ~/.ssh/id_ecdsa.pub >> authorized_keys
+[ -r ~/.ssh/id_ed25519.pub ] && cat ~/.ssh/id_ed25519.pub >> authorized_keys
 [ -r ~/.ssh/id_rsa.pub ] && cat ~/.ssh/id_rsa.pub >> authorized_keys
 [ -r ~/.ssh/authorized_keys ] && cat ~/.ssh/authorized_keys >> authorized_keys
 
