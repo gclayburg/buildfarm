ecbc45578f5a5b183520d4514caca6e2dcae67eb
Gary Clayburg <gclaybur@comcast.net>
2016-07-09 15:27:57 -0600

    added ability to pass flags onto docker-compose rm

--- wipedata.sh
+++ wipedata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker-compose stop
-docker-compose rm jenkinsmaster jenkinsnginx
-docker-compose rm -v jenkinsdata
-docker-compose rm -v jslavedata
-docker-compose rm -v sshdata
+docker-compose rm "$@" jenkinsmaster jenkinsnginx
+docker-compose rm -v "$@" jenkinsdata
+docker-compose rm -v "$@" jslavedata
+docker-compose rm -v "$@" sshdata
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null)
 docker rmi $(docker images -q --filter="dangling=true")
