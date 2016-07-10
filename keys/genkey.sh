cf76344a86d25de784c4189f218125c8f9df05e3
Gary Clayburg <gclaybur@comcast.net>
2016-07-10 10:34:54 -0600

    set sensible permissions for ssh folder on key generation

--- keys/genkey.sh
+++ keys/genkey.sh
@@ -2,6 +2,7 @@
 HOMEDIR=$(dirname $0)
 cd $HOMEDIR
 ssh-keygen -t rsa -b 4096 -C jenkins-master -f jenkins-master-key -N ""
+
 cp jenkins-master-key ../jenkins-data/.ssh/id_rsa
 cp jenkins-master-key.pub ../jenkins-data/.ssh/id_rsa.pub
 
@@ -14,6 +15,9 @@ cat jenkins-master-key.pub >> authorized_keys
 [ -r ~/.ssh/id_rsa.pub ] && cat ~/.ssh/id_rsa.pub >> authorized_keys
 [ -r ~/.ssh/authorized_keys ] && cat ~/.ssh/authorized_keys >> authorized_keys
 
+chmod 700 ../jenkins-data/.ssh
+chmod 700 ../ssh-data/.ssh
+chmod 700 ../jslave-data/.ssh
 cp authorized_keys ../ssh-data/.ssh/
 cp authorized_keys ../jslave-data/.ssh/
 
