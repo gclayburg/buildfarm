6fd42a2541d6f113e45f0b4f875fef4ad59f2f16
Gary Clayburg <gclaybur@comcast.net>
2016-07-15 19:03:33 -0600

    added hostname arg

--- ssh-data/createrepo.sh
+++ ssh-data/createrepo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NEWREPO=$1
+DOCKERHOST=$2
 cd /home/git && git init --bare $NEWREPO.git
-cat post-receive-template | sed -e "s/BUILDFARMHOST/scranton2/g" -e "s/REPONAME/$NEWREPO/g" > ${NEWREPO}.git/hooks/post-receive
+cat post-receive-template | sed -e "s/BUILDFARMHOST/${DOCKERHOST}/g" -e "s/REPONAME/$NEWREPO/g" > ${NEWREPO}.git/hooks/post-receive
 chmod 755 ${NEWREPO}.git/hooks/post-receive
